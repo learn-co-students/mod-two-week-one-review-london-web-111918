507578135bad90856ea80b15806eb55d3b403100
IMH51 <ianmartinharrison@outlook.com>
2018-12-14 16:39:54 +0000

    Migrated sinatra seven seas app to run in rails

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -1,6 +1,42 @@
 class SeasController < ApplicationController
   #define your controller actions here
 
+  def index
+    @seas = Sea.all
+    render 'index'
+  end
+
+  def show
+    @sea = Sea.find(params[:id])
+  end
+
+  def edit
+    @sea = Sea.find(params[:id])
+  end
+
+  def update
+    @sea = Sea.find(params[:id])
+    @sea.update(sea_params)
+    render 'show'
+  end
+
+  def new
+    @sea = Sea.new
+    render 'new'
+  end
+
+  def create
+    Sea.create(sea_params)
+    @seas = Sea.all
+    render 'index'
+  end
+
+  def destroy
+    @sea = Sea.find(params[:id])
+    @sea.destroy
+    @seas = Sea.all
+    render 'index'
+  end
 
   private
   # In controller actions, use this private method to access sea params from forms.
